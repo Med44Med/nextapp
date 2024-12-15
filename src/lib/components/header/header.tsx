1521099c5d783a381b879ab21786dabef8182f76
Med44Med <mstgci4@gmail.com>
2024-12-15 07:22:09 +0100

    next 31

--- src/lib/components/header/header.tsx
+++ src/lib/components/header/header.tsx
@@ -6,14 +6,14 @@ import HeaderLoginStatus from './headerLoginStatus';
 function Header() {
 
   return (
-    <header className="fixed w-full h-20 bg-transparent z-10 flex items-center justify-start transition-all delay-100">
-        <div className="h-full w-1/5 bg-red-500">a</div>
-        <nav className="flex flex-row grow w-full justify-around items-center">
+    <header className="fixed w-full h-20 px-4 bg-transparent z-10 flex items-center justify-start transition-all delay-100">
+        <div className="h-full w-1/5 bg-white text-black flex justify-center items-center">Dorossi</div>
+        <nav className="flex flex-row grow w-full justify-evenly items-center">
             <Link href='/'>Link1</Link>
             <Link href='/'>Link2</Link>
             <Link href='/'>Link3</Link>
         </nav>
-        <div className="h-full w-1/5 flex justify-around items-center bg-red-500">
+        <div className="h-full grow w-1/4 flex justify-evenly items-center gap-3">
           <ThemeHandler />
           <HeaderLoginStatus />
         </div>
--- src/lib/components/header/headerLoginStatus.tsx
+++ src/lib/components/header/headerLoginStatus.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import {useEffect,useState} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { IoMdArrowDropdown } from "react-icons/io";
 
 
 
@@ -11,31 +12,30 @@ import Image from 'next/image';
 function HeaderLoginStatus() {
 
     const [session, setSession] = useState()
+    const [activeMenu, setActiveMenu] = useState(false)
 
     useEffect(() => {
         setSession(JSON.parse(window.localStorage.getItem("session")))
-    }, [session])
-
-    console.log(session);
+    }, [])
+    
     
     
     if(!session) {  return (
-            <div>
+            <div className="flex justify-center items-center gap-3">
                 <Link href='/login' className="font-bold text-lg font-main">تسجيل الدخول</Link>
-                <Link href='/register' className="font-bold text-lg font-main">إنشاء حساب</Link>
+                <Link href='/register' className="font-bold text-lg font-main bg-main rounded p-3 text-foreground">إنشاء حساب</Link>
             </div>
     )} else {
         return (
           
-                <div className="relative flex w-1/5 items-center jsustify-center">
-                    <div>
-                        <Image src={session.avatar} alt="profil picture" height={30} width={30} style={{borderRadius:'50%',height:'30px',width:'30px'}}/>
-                        <h1>{session.username}</h1>
-                    </div>
-                    <div className="absolute top-[105%] left-0 w-full ">
-                        <Link href='/dashboard'>Dashboard</Link>
-                        <Link href='/profil'>Profil</Link>
-                        <Link href='/'>Log out</Link>
+                <div className="group grow relative flex flex-row h-full items-center justify-center cursor-pointer">
+                    <Image src={session.avatar} alt="profil picture" height={30} width={30} style={{borderRadius:'50%',height:'30px',width:'30px'}}/>
+                    <h1>{session.username}</h1>
+                    <IoMdArrowDropdown className="transition-all group-hover:rotate-180" />
+                    <div className="absolute bg-foreground text-background top-[105%] left-0 w-full overflow-hidden rounded transition-all flex flex-col items-center gap-3 max-h-0 p-0 opacity-0 group-hover:max-h-fit group-hover:p-3 group-hover:opacity-100">
+                        <Link href='/dashboard' className="px-3 py-1 rounded transition-colors w-full text-center hover:bg-main hover:text-foreground">Dashboard</Link>
+                        <Link href='/profil' className="px-3 py-1 rounded transition-colors w-full text-center hover:bg-main hover:text-foreground">Profil</Link>
+                        <Link href='/' className="px-3 py-1 rounded transition-colors w-full text-center hover:bg-main hover:text-foreground">Log out</Link>
                     </div>
 
                 </div>
